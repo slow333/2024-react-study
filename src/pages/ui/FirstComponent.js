2ddda08bb6115a4905fe686db4d7d3a44ed8d606
limp bizkit <slow33@gmail.com>
2024-10-06 09:16:20 +0900

    first commit

---
+++ src/pages/ui/FirstComponent.js
@@ -0,0 +1,7 @@
+export default function FirstComonent(){
+  return(
+      <div className='main'>
+        <h3>첫번째 component</h3>
+      </div>
+  )
+}
---
+++ src/pages/ui/UiRender.js
@@ -0,0 +1,17 @@
+export default function UiRender() {
+  return (
+      <div className='sub-nav'>
+        <h2>Ui rendering 관련 페이지</h2>
+        <nav>
+          <ul>
+            <li>
+              
+            </li>
+          </ul>
+        </nav>
+        <div className='main'>
+        </div>
+
+      </div>
+  )
+}
